afbe59e27e5eba099e9e5e0b7853dc1cdec0ea6c
audric <audric@debian>
2013-05-18 15:37:23 +0200

    first commit particuleGenerateur

---
+++ Core/include/ParticuleGenerateur.h
@@ -0,0 +1,67 @@
+#ifndef _PARTICULE_GENERATEUR_H_
+#define _PARTICULE_GENERATEUR_H_
+
+
+#include <vector>
+#include "Vectors.h"
+#include "Particule.h"
+
+#define DEFAULT_RADIUS 100
+#define DEFAULT_CENTER Vec3i(0,0,0)
+#define DEFAULT_NB_ITEM 100
+#define DEFAULT_LIFETIME_MIN 100
+#define DEFAULT_LIFETIME_MAX 500
+#define DEFAULT_SIZE_MIN 10
+#define DEFAULT_SIZE_MAX 100
+
+
+using namespace std;
+
+/* 	Cette classe permet de génerer des particules avec certaines propriétés.
+	La forme de ce générateur est par défault un cercle dans le plan XoY */
+class ParticuleGenerateur {
+private:
+	/* rayon du cercle sur lequel sont génerées les particules*/
+	float radius;
+	
+	/* position du centre du cercle */
+	Vec3i center;
+	
+	/* nombre de particules de ce générateur. Les particules 
+	sont réutilisées après leurs mort afin que l'on ai toujours 
+	le même nombre de particules */
+	int nbItem;
+
+	/* nombre de particules vivante actuellement. */
+	int nbAlive;
+
+	/* durée de vie minium des particules générées */
+	float lifeTimeMin;
+
+	/* durée de vie maximum des particules générées */
+	float lifeTimeMax;
+
+	/* taille minimum des particules générées */
+	float sizeMin;
+
+	/* taille maximum des particules générées */
+	float sizeMax;
+
+	/* liste des particules de ce générateur, vivantes ou mortes */
+	vector<Particule> particles;
+
+	/* 	liste des particules actuellement vivantes, donc les seules dont on doit
+		effectuer le rendu */
+	vector<Particule*> alive;
+
+public:
+	ParticuleGenerateur();
+	ParticuleGenerateur(float _radius, Vec3i _center, int _nbItem, float _lifeTimeMin, float _lifeTimeMax, float _sizeMin, float _sizeMax);
+	~ParticuleGenerateur();
+
+};
+
+
+
+
+#endif
---
+++ Core/src/ParticuleGenerateur.cpp
@@ -0,0 +1,34 @@
+
+#include "ParticuleGenerateur.h"
+
+#include "Particule.h"
+#include "Vectors.h"
+
+
+ParticuleGenerateur::ParticuleGenerateur() : 
+	ParticuleGenerateur(DEFAULT_RADIUS, DEFAULT_CENTER, DEFAULT_NB_ITEM,
+		(float) DEFAULT_LIFETIME_MIN, (float) DEFAULT_LIFETIME_MAX, (float) DEFAULT_SIZE_MIN,
+		(float) DEFAULT_SIZE_MAX)
+{
+
+}
+
+
+ParticuleGenerateur::ParticuleGenerateur(
+	float _radius, Vec3i _center, int _nbItem, float _lifeTimeMin, 
+	float _lifeTimeMax, float _sizeMin, float _sizeMax) : 
+		radius(_radius), center(_center), nbItem(_nbItem),
+		lifeTimeMin(_lifeTimeMin), lifeTimeMax(_lifeTimeMax),
+		nbAlive(), sizeMin(_sizeMin), sizeMax(_sizeMax),
+		alive(_nbItem,NULL) 
+{
+	//particles();
+	//particles.reserve(nbItem);
+}
+
+
+ParticuleGenerateur::~ParticuleGenerateur() {
+}
+
+
+
