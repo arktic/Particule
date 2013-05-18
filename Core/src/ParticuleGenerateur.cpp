cebcc0d688dfb111d21cbafc82ce0be20fa1cb6d
arktic17 <arktic17@gmail.com>
2013-05-18 16:00:31 +0200

    erzs

--- Core/src/ParticuleGenerateur.cpp
+++ Core/src/ParticuleGenerateur.cpp
@@ -5,21 +5,27 @@
 #include "Vectors.h"
 
 /*
-ParticuleGenerateur::ParticuleGenerateur() : 
-	ParticuleGenerateur(DEFAULT_RADIUS, DEFAULT_CENTER, DEFAULT_NB_ITEM,
-		(float) DEFAULT_LIFETIME_MIN, (float) DEFAULT_LIFETIME_MAX, (float) DEFAULT_SIZE_MIN,
-		(float) DEFAULT_SIZE_MAX)
+ParticuleGenerateur::ParticuleGenerateur() :
+    ParticuleGenerateur(DEFAULT_RADIUS, DEFAULT_CENTER, DEFAULT_NB_ITEM,
+        (float) DEFAULT_LIFETIME_MIN, (float) DEFAULT_LIFETIME_MAX, (float) DEFAULT_SIZE_MIN,
+        (float) DEFAULT_SIZE_MAX)
 {
 
 }*/
 
 
-ParticuleGenerateur::ParticuleGenerateur(
-	float _radius, Vec3i _center, int _nbItem, float _lifeTimeMin, 
-	float _lifeTimeMax, float _sizeMin, float _sizeMax) : 
-		radius(_radius), center(_center), nbItem(_nbItem),
-		lifeTimeMin(_lifeTimeMin), lifeTimeMax(_lifeTimeMax),
-		nbAlive(), sizeMin(_sizeMin), sizeMax(_sizeMax),
+ParticuleGenerateur::ParticuleGenerateur
+(  float _radius     ,  Vec3i _center , int _nbItem  , float _lifeTimeMin,
+   float _lifeTimeMax,  float _sizeMin, float _sizeMax
+ ):
+        radius(_radius),
+        center(_center),
+        nbItem(_nbItem),
+        lifeTimeMin(_lifeTimeMin),
+        lifeTimeMax(_lifeTimeMax),
+        nbAlive(),
+        sizeMin(_sizeMin),
+        sizeMax(_sizeMax),
         alive(_nbItem,(Particule*)NULL)
 {
     //particles();
