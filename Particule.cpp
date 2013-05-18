2ae5818ea69e8fe1747fe8a47d3f935c115fdf6c
audric <audric@debian>
2013-05-18 10:52:21 +0200

    add getter & setter for particule

---
+++ Particule.cpp
@@ -0,0 +1,39 @@
+int Particule::getAge() const {
+	return age;
+}
+
+void Particule::setAge(int age) {
+	this->age = age;
+}
+
+int Particule::getLifeTime() const {
+	return lifeTime;
+}
+
+void Particule::setLifeTime(int lifeTime) {
+	this->lifeTime = lifeTime;
+}
+
+vec3 Particule::getPosition() const {
+	return position;
+}
+
+void Particule::setPosition(vec3 position) {
+	this->position = position;
+}
+
+int Particule::getSize() const {
+	return size;
+}
+
+void Particule::setSize(int size) {
+	this->size = size;
+}
+
+vec3 Particule::getSpeed() const {
+	return speed;
+}
+
+void Particule::setSpeed(vec3 speed) {
+	this->speed = speed;
+}
--- Particule.h
+++ Particule.h
@@ -1,15 +1,24 @@
 class Particule {
 protected:
-    int         lifeTime;
-    int         age;
-    int         size;
-    vec3        position;
-    vec3        speed;
+	int lifeTime;
+	int age;
+	int size;
+	vec3 position;
+	vec3 speed;
 public:
-    Particule();
-    Particule(int lifeTime, int size, vec3 pos, vec3 speed);
+	Particule();
+	Particule(int lifeTime, int size, vec3 pos, vec3 speed);
 
-    void Render();
-    bool isALive();
-
+	void Render();
+	bool isALive();
+	int getAge() const;
+	void setAge(int age);
+	int getLifeTime() const;
+	void setLifeTime(int lifeTime);
+	vec3 getPosition() const;
+	void setPosition(vec3 position);
+	int getSize() const;
+	void setSize(int size);
+	vec3 getSpeed() const;
+	void setSpeed(vec3 speed);
 };
